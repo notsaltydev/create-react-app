d4ab9f1b0b32dc86ed71ed5e7ebf309c9bb0604b
notsaltydev <notsaltydev@gmail.com>
2019-07-18 20:03:09 +0200

    Updates

---
+++ precache-manifest.5de8ea6a006b4d27b73ae494fbcf033c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e58c65f48da3f89553878ab75a79fb68",
+    "url": "/create-react-app/index.html"
+  },
+  {
+    "revision": "4700e74c8984ee21558f",
+    "url": "/create-react-app/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/create-react-app/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "4700e74c8984ee21558f",
+    "url": "/create-react-app/static/js/main.10ff1247.chunk.js"
+  },
+  {
+    "revision": "ee6d20426387d02678ad",
+    "url": "/create-react-app/static/js/runtime~main.7a134d5e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/create-react-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
